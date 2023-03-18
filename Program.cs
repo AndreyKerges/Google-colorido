f8ccb92215dc9000e09062ce0fd31f2aa9f7dae3
AndreyKerges <andrey.nascimento>
2023-03-17 21:22:48 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+// Muda a cor da letra 
+Console.ForegroundColor = ConsoleColor.Blue;
+// Escreve a letra G
+Console.Write("G");
+// Delay de 1 segundo para exibição
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("O");
+Thread.Sleep(1000);     
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("O");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("G");
+Thread.Sleep(1000);     
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("L");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("E");
+Thread.Sleep(1000);
+
+Console.ResetColor();
+
